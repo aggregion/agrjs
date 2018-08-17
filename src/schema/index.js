5126c4f1894a46c3b277391f480f74b1c4522ce5
Denis Bezrukov <anthrax63@gmail.com>
2018-08-17 16:33:26 +0300

    initial changes

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,8 +1,8 @@
 const schema = Object.assign(
-  {},
-  require('./chain_types.json'),
-  require('./eosio_system.json'),
-  require('./eosio_token.json')
-)
+    {},
+    require('./chain_types.json'),
+    require('./eosio_system.json'),
+    require('./eosio_token.json')
+);
 
-module.exports = schema
+module.exports = schema;
